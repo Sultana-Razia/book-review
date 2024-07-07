da257ea694032741ac9054ee431580f65e27f049
Sultana Razia <sultanarazia483@gmail.com>
2024-07-07 13:21:38 +0600

    display all books

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,9 +1,31 @@
-
+import { CiStar } from "react-icons/ci";
 
 const Book = ({ book }) => {
+    const { image, bookName, author, category, rating, tags } = book
     return (
-        <div>
-            <h2>{book.bookName}</h2>
+        <div className="w-[374px] h-[481px] border rounded-2xl p-6">
+            <div className="card shadow-xl">
+                <figure className="w-[326px] h-[230px] bg-[#F3F3F3] rounded-2xl">
+                    <img
+                        className="w-[134px] h-[166px]"
+                        src={image}
+                        alt="Book" />
+                </figure>
+                <div className="card-body mt-6 pt-0 h-[180px]">
+                    <div className="text-[#23BE0A] font-medium text-base">
+                        {tags.map(tag => <span className="bg-[#23BE0A0D] rounded-3xl py-1 px-4">{tag}</span>)
+                        }
+                    </div>
+                    <h2 className="card-title text-[#131313] playfairFont font-bold py-2">
+                        {bookName}
+                    </h2>
+                    <p className="text-[#131313CC] font-medium mb-3">By: {author}</p>
+                    <div className="card-actions justify-between text-[#131313CC] font-medium">
+                        <div className="">{category}</div>
+                        <div className="flex items-center gap-1">{rating} <CiStar /></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -14,9 +14,9 @@ const Books = () => {
     return (
         <div>
             <h2 className="mt-24 text-[#131313] font-bold text-4xl text-center">Books</h2>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-items-center mt-10">
                 {
-                    books.map(book => <Book key={book.id} book={book}></Book>)
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
                 }
             </div>
         </div>
